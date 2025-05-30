08d330237bd20c57ffccc97d2f76d8ad6425163f
SamuelRonas <samuelronas17@gmail.com>
2025-05-29 22:51:31 -0300

    ✨ feat: Componente-List
    testando algumas funcionalidades do framer-motion principalmente o Reorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { motion, AnimatePresence, Reorder } from 'framer-motion';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+
+  function list() {
+    const [caixas, setCaixas] = useState(["Caixa 1", "Caixa 2", "Caixa 3"]);
+
+    return (
+      <div className='grid'>
+        <Reorder.Group axis='x' values={caixas} className="reorder-group" onReorder={setCaixas}>
+          {caixas.map((caixa, i) => (
+            <Reorder.Item key={caixa} value={caixa}>
+              <button className='caixa'>
+                <p>{caixa}</p>
+              </button>
+            </Reorder.Item>
+          ))}
+
+        </Reorder.Group>
+      </div>
+    );
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    list()
+  );
 }
 
 export default App
