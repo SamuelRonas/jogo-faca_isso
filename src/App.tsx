54b49279bf918a9ca1ccd280af980e2249b7f011
SamuelRonas <samuelronas17@gmail.com>
2025-05-31 14:32:17 -0300

    🦄 refactor: Componente UI
    Criando o componente interface do usuario que abriga toda a logica de arrastar as caixas

--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,19 @@
 // App.tsx
-import { act, useState } from 'react';
-import './App.css';
-import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
-import { Caixa } from './components/caixa/caixa';
-import { motion } from 'framer-motion';
 
-interface CaixaType {
-  id: string;
-  palavra: string;
-}
 
-function constroiCaixasPadrao(): CaixaType[] {
-  return [
-    { id: '1', palavra: 'Abrir' },
-    { id: '2', palavra: 'lrbhfweufhefhweuiofhe' },
-    { id: '3', palavra: 'Mover' },
-    { id: '4', palavra: 'Movrttrter' },
-    { id: '5', palavra: 'Mover' },
-    { id: '6', palavra: 'Mover' },
-    { id: '7', palavra: 'yhytjhyytjyjy' },
-    { id: '8', palavra: 'Moukukver' },
-    { id: '9', palavra: 'Movetgeeer' },
-    { id: '10', palavra: 'Mover' },
-    { id: '11', palavra: 'Movyjyjyter' },
-    { id: '12', palavra: 'Moveyjhtyjtjtyjr' },
-    { id: '13', palavra: 'Movyjtyjtyjtjtyjyer' },
-    { id: '14', palavra: 'Movyjtyjyjtyjer' },
-    { id: '15', palavra: 'Movrhjyjyjyttyjter' },
-    { id: '16', palavra: 'Morthtuver' },
-    { id: '17', palavra: 'Mover' },
-    { id: '18', palavra: 'Movyjtyjer' },
-    { id: '19', palavra: 'Abrir' },
-    { id: '20', palavra: 'lrbhfweufhefhweuiofhe' },
-    { id: '21', palavra: 'Movrgergrreger' },
-    { id: '22', palavra: 'Morstghtghrhsver' },
-    { id: '23', palavra: 'Moverthstttttttttr' },
-    { id: '24', palavra: 'Myh5hhover' },
-    { id: '25', palavra: 'Mogyetnertgertnergmkrver' },
-    { id: '26', palavra: 'Movgreg regergrgergergeger' },
-    { id: '27', palavra: 'uifohsuofhishfsd' },
-    { id: '28', palavra: 'aeevvveeveve' },
-    { id: '29', palavra: 'aevvvvv' },
-    { id: '30', palavra: 'aveeeee' },
-    { id: '31', palavra: 'aevvvvvvvv' },
-    { id: '32', palavra: 'aveeeeeeeeeeveve' },
-    { id: '33', palavra: 'ajjjjyttttttttttttttttttttttttttj' },
-    { id: '34', palavra: 'agrggrghhhdgh' },
-    { id: '35', palavra: 'ahghghghgh' },
-    { id: '36', palavra: 'ayjjjjjjjjjjjjjjjjjjjjjjjjjjjjj' },
-    { id: '38', palavra: 'ghgfhfghfghfghghfgha' },
-    { id: '39', palavra: 'aghfghfghgfhgg' },
-    { id: '40', palavra: 'aghfghfhfghfg' },
-  ];
-}
+import { InterfaceUsuario } from './components/interfaceUsuario/interfaceUsuario';
+
+
 
 function App() {
-  const [caixasDisponiveis, setCaixasDisponiveis] = useState<CaixaType[]>(constroiCaixasPadrao());
-  const [caixasNaLinha, setCaixasNaLinha] = useState<CaixaType[]>([]);
 
-  function onDragEnd(event: any) {
-    const { active, over } = event;
-    console.log(active.id + "        " + over.id)
-    if (over && over.id === 'linhaCodigo') {
-      const caixaArrastada = caixasDisponiveis.find((caixa) => caixa.id === active.id);
-      if (caixaArrastada) {
-        setCaixasNaLinha((prev) => [...prev, caixaArrastada]);
-        setCaixasDisponiveis((prev) => prev.filter((c) => c.id !== active.id));
-      }
-    }
-    if (over && over.id === 'areaDisponivel') {
-      const caixaArrastada = caixasNaLinha.find((caixa) => caixa.id === active.id);
-      if (caixaArrastada) {
-        setCaixasNaLinha((prev) => prev.filter((c) => c.id !== active.id));
-        setCaixasDisponiveis((prev) => [...prev, caixaArrastada])
-      }
-    }
-
-  }
-
-  return (
-    <>
-      <div className='view'></div>
-      <h2 className='nome'>Faça Isso</h2>
-      <DndContext onDragEnd={onDragEnd}>
-        <LinhaCodigo caixasNaLinha={caixasNaLinha} />
-        <AreaDisponivel caixasDisponiveis={caixasDisponiveis} />
-      </DndContext>
-    </>
-  );
-}
-
-function AreaDisponivel({ caixasDisponiveis }: { caixasDisponiveis: CaixaType[]; }) {
-  const { setNodeRef } = useDroppable({ id: 'areaDisponivel' });
-
-  return (
-    <div ref={setNodeRef} className="areaDisponivel">
-      <div className='gridInterno'>
-        {caixasDisponiveis.map((caixa) => (
-          <Caixa key={caixa.id} caixa={caixa} />
-        ))}
-      </div>
-    </div>
+  return(
+    <InterfaceUsuario/>
   )
-
+ 
 }
 
-function LinhaCodigo({ caixasNaLinha }: { caixasNaLinha: CaixaType[]; }) {
-  const { setNodeRef } = useDroppable({ id: 'linhaCodigo' });
 
-  return (
-    <div ref={setNodeRef} className="container">
-      <div className='gridInterno'>
-        {caixasNaLinha.map((caixa) => (
-          <Caixa key={caixa.id} caixa={caixa} />
-        ))}
-      </div>
-    </div>
-  );
-}
 
 
 
